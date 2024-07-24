d4f2e1c9950d4c030ff86d4e144a046375ac25ad
sneha-at-hub <snehatmg2004@gmail.com>
2024-07-24 08:31:42 +0545

    User create, edit, and get)

--- library/libraryapi/urls.py
+++ library/libraryapi/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
    path('users/', views.user_list, name="user_list"),
+   path('create/users/', views.create_user, name="create_user"),
+   path('update/users/<int:pk>/', views.update_user, name="update_user"),
 ]
--- library/libraryapi/views.py
+++ library/libraryapi/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from libraryapi.models import User
-from libraryapi.serializers import UserSerializer
+from libraryapi.models import User, Student
+from libraryapi.serializers import UserSerializer, StudentSerializer
 
 
 # Create your views here.
@@ -10,3 +10,25 @@ def user_list(request):
     queryset = User.objects.all()
     serializer = UserSerializer(queryset, many=True)
     return Response(serializer.data)
+
+
+@api_view(['PATCH'])
+def update_user(request, pk):
+    queryset = User.objects.get(pk=pk)
+    data = UserSerializer(instance=queryset, data=request.data)
+    
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    else:
+        return Response(data.errors)
+    
+@api_view(['POST'])
+def create_user(request):
+    serializer=UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors)
+    
