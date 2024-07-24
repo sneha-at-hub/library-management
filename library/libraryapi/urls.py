e6926207b43de883c6d4ab4d62e53fe0d354093f
sneha-at-hub <snehatmg2004@gmail.com>
2024-07-24 12:21:44 +0545

    Complete CRUD of library management system

--- library/libraryapi/urls.py
+++ library/libraryapi/urls.py
@@ -6,14 +6,45 @@ urlpatterns = [
    path('create/users/', views.create_user, name="create_user"),
    path('update/users/<int:pk>/', views.update_user, name="update_user"),
    path('user/<int:pk>/delete/', views.deleteUser, name="deleteUser"),
+   
+   
    path('students/', views.getStudents, name="getStudents"),
    path('students/create/', views.createStudents, name="createStudents"),
    path('students/update/<int:pk>/', views.updateStudents, name="updateStudents"),
+   
+   
    path('authors/', views.getAuthors, name="getAuthors"),
    path('authors/create/', views.createAuthours, name="createAuthours"),
    path('authors/update/<int:pk>/', views.updateAuthors, name="updateAuthors"),
+   
+   
    path('category/create/', views.createCategory, name="createCategory"),
    path('category/update/<int:pk>', views.updateCategory, name="updateCategory"),
    path('category/', views.getcategory, name="getcategory"),
    path('category/<int:pk>/delete/', views.deleteCategory, name="deleteCategory"),
+   
+   
+   path('publisher/create/', views.createPublisher, name="createPublisher"),
+   path('publisher/update/<int:pk>', views.updatePublisher, name="updatePublisher"),
+   path('publisher/', views.getPublisher, name="getPublisher"),
+   path('publisher/<int:pk>/delete/', views.deletePublisher, name="deletePublisher"),
+   
+   path('books/create', views.createBooks, name="createBooks"),
+   path('books/', views.getBooks, name="getBooks"),
+   path('books/update/<int:pk>', views.updateBooks, name="updateBooks"),
+   path('books/<int:pk>/delete/', views.deleteBooks, name="deleteBooks"),
+   
+   
+   path('borrowing/create', views.createBorrowing, name="createBorrowing"),
+   path('borrowing/', views.getBorrowing, name="getBorrowing"),
+   path('borrowing/update/<int:pk>', views.updateBorrowing, name="updateBorrowing"),
+   path('borrowing/<int:pk>/delete/', views.deleteBorrowing, name="deleteBorrowing"),
+   
+   path('reservation/create', views.createReservation, name="createReservation"),
+   path('reservation/', views.getReservation, name="getReservation"),
+   path('reservation/update/<int:pk>', views.updateReservation, name="updateReservation"),
+   path('reservation/<int:pk>/delete/', views.deleteReservation, name="deleteReservation"),
+   
+   
+   
 ]
--- library/libraryapi/views.py
+++ library/libraryapi/views.py
@@ -2,11 +2,15 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from libraryapi.models import User, Student, Author, Category
-from libraryapi.serializers import UserSerializer, StudentSerializer, AuthorSerializer, CategorySerializer
+from libraryapi.models import User, Student, Author, Category, Publisher, Book, Borrowing, Reservation
+from libraryapi.serializers import UserSerializer, StudentSerializer, AuthorSerializer, CategorySerializer, PublisherSerializer, BookSerializer, BorrowingSerializer, ReservationSerializer
 
 
 # Create your views here.
+
+# ==========================================        =========================================== 
+#                                            USERS
+# ==========================================       ===========================================
 @api_view(['GET'])
 def user_list(request):
     queryset = User.objects.all()
@@ -41,7 +45,16 @@ def deleteUser(request, pk):
     user.delete()
     return Response({"message": "User deleted successfully"}, status=status.HTTP_202_ACCEPTED)
     
-#for students
+    
+    
+    
+# ==========================================        =========================================== 
+#                                            STUDENTS
+# ==========================================       ===========================================
+
+
+
+
 @api_view(['GET'])
 def getStudents(request):
     student = Student.objects.all()
@@ -97,7 +110,13 @@ def updateAuthors(request, pk):
     return Response(serializer.errors)
 
 
-#for category
+
+
+
+
+# ==========================================            =========================================== 
+#                                             CATEGORY
+# ==========================================            ===========================================
 @api_view(['GET'])
 def getcategory(request):
     data = Category.objects.all()
@@ -125,5 +144,148 @@ def updateCategory(request, pk):
 @api_view(['DELETE'])
 def deleteCategory(request, pk):
     data = get_object_or_404(Category, pk=pk)
+    data.delete()
+    return Response({"message": "Deleted data successfully"}, status=status.HTTP_202_ACCEPTED)
+
+
+
+
+# ==========================================            =========================================== 
+#                                            PUBLICATION
+# ==========================================            ===========================================
+
+
+
+
+@api_view(['GET'])
+def getPublisher(request):
+    data = Publisher.objects.all()
+    serializer = PublisherSerializer(data, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def createPublisher(request):
+    serializer = PublisherSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+@api_view(['PATCH'])
+def updatePublisher(request, pk):
+    category = Publisher.objects.get(pk=pk)
+    serializer = PublisherSerializer(instance=category, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+    
+@api_view(['DELETE'])
+def deletePublisher(request, pk):
+    data = get_object_or_404(Publisher, pk=pk)
+    data.delete()
+    return Response({"message": "Deleted data successfully"}, status=status.HTTP_202_ACCEPTED)
+
+
+# ==========================================            =========================================== 
+#                                              BOOKS
+# ==========================================            ===========================================
+
+@api_view(['POST'])
+def createBooks(request):
+    serializer = BookSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+@api_view(['GET'])
+def getBooks(request):
+    data = Book.objects.all()
+    serializer = BookSerializer(data, many=True)
+    return Response(serializer.data)
+
+@api_view(['PATCH'])
+def updateBooks(request, pk):
+    data = Book.objects.get(pk=pk)
+    serializer = BookSerializer(instance=data, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+@api_view(['DELETE'])
+def deleteBooks(request, pk):
+    data = get_object_or_404(Book, pk=pk)
+    data.delete()
+    return Response({"message": "Deleted data successfully"}, status=status.HTTP_202_ACCEPTED)
+    
+
+
+# ==========================================            =========================================== 
+#                                            BORROWINGS
+# ==========================================            ===========================================
+
+@api_view(['POST'])
+def createBorrowing(request):
+    serializer = BorrowingSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+@api_view(['GET'])
+def getBorrowing(request):
+    data = Borrowing.objects.all()
+    serializer = BorrowingSerializer(data, many=True)
+    return Response(serializer.data)
+
+@api_view(['PATCH'])
+def updateBorrowing(request, pk):
+    data = Borrowing.objects.get(pk=pk)
+    serializer = BorrowingSerializer(instance=data, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+@api_view(['DELETE'])
+def deleteBorrowing(request, pk):
+    data = get_object_or_404(Borrowing, pk=pk)
+    data.delete()
+    return Response({"message": "Deleted data successfully"}, status=status.HTTP_202_ACCEPTED)
+
+
+# ==========================================            =========================================== 
+#                                            RESERVATION
+# ==========================================            ===========================================
+
+@api_view(['POST'])
+def createReservation(request):
+    serializer = ReservationSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+@api_view(['GET'])
+def getReservation(request):
+    data = Reservation.objects.all()
+    serializer = ReservationSerializer(data, many=True)
+    return Response(serializer.data)
+
+@api_view(['PATCH'])
+def updateReservation(request, pk):
+    data = Reservation.objects.get(pk=pk)
+    serializer = ReservationSerializer(instance=data, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+@api_view(['DELETE'])
+def deleteReservation(request, pk):
+    data = get_object_or_404(Reservation, pk=pk)
     data.delete()
     return Response({"message": "Deleted data successfully"}, status=status.HTTP_202_ACCEPTED)
